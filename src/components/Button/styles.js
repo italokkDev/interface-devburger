d96ac8b362c2e36eb6e69122e2e26a08eb79b3c7
italokkDev <dev.italoramon@gmail.com>
2024-11-15 22:47:23 -0300

    atualizando o background-image

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -14,21 +14,11 @@ export const ContainerButton = styled.button`
     &:hover{
        border-radius: 5px;
        background-color: #6f357c;
-       background-image: url("data:image/svg+xml,%3csvg 
-                              width='100%25' 
-                              height='100%25' 
-                              xmlns='http://www.w3.org/2000/svg'%3e%3crect 
-                              width='100%25' 
-                              height='100%25' 
-                              fill='none' 
-                              rx='5' 
-                              ry='5' 
-                              stroke='white' 
-                              stroke-width='3' 
-                              stroke-dasharray='6%2c 14' 
-                              stroke-dashoffset='0' 
-                              stroke-linecap='square'/%3e%3c/svg%3e");
+       background-image: url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' rx='5' ry='5' stroke='white' stroke-width='3' stroke-dasharray='6%2c 14' stroke-dashoffset='0' stroke-linecap='square'/%3e%3c/svg%3e");
+       
 
     }
 
-`
+`;
+
+
